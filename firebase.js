9e99200cb6dc1e75ab6c906a62aa6663acfbba31
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-27 11:51:14 -0400

    feat(firebase): update firbase api key details

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAI1brKkR9XfOGErHdMQKyfj8GQbKR7tXA",
-    authDomain: "to-do-list-519ae.firebaseapp.com",
-    projectId: "to-do-list-519ae",
-    storageBucket: "to-do-list-519ae.appspot.com",
-    messagingSenderId: "897143349984",
-    appId: "1:897143349984:web:e23f665d2848abc8fa2685",
-    measurementId: "G-N6YDZCPM2S"
+    apiKey: "AIzaSyA0Rbp35cPeWo-jBTZBjvAiTEbb1aPxymw",
+    authDomain: "to-do-app-f4bc6.firebaseapp.com",
+    projectId: "to-do-app-f4bc6",
+    storageBucket: "to-do-app-f4bc6.appspot.com",
+    messagingSenderId: "888856971912",
+    appId: "1:888856971912:web:a25e8f8ea00506827e2d3d",
+    measurementId: "G-53YYSQNVP4"
 };
 
 firebase.initializeApp(firebaseConfig);
